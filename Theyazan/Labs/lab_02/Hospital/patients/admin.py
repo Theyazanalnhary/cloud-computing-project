f6eab54ca94da7ffeb2ca6cc78f0a41ba88d497b
Theyazanalnhary <dhyyznqasm23@gmail.com>
2025-04-29 00:37:45 +0300

    Delete Theyazan/Labs/lab_02/Hospital directory

--- Theyazan/Labs/lab_02/Hospital/patients/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . models  import  Patients
-# Register your models here.
-
-admin.site.site_header='H.P.S'
-admin.site.site_title='H.PS'
-admin.site.register(Patients)
--- Theyazan/Labs/lab_02/Hospital/user/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # رابط لوحة التحكم الخاصة بدجانغو
-    path('user/', include('user.urls')),  # تضمين روابط تطبيق المستخدم
-]
