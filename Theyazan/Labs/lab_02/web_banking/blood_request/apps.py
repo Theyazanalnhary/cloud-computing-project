7e3c008034ad7cc8e397d4bd7f119ee3b9c824b9
Theyazanalnhary <dhyyznqasm23@gmail.com>
2025-04-29 00:43:58 +0300

    Delete Theyazan/Labs/lab_02/web_banking directory

--- Theyazan/Labs/lab_02/web_banking/blood_request/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BloodRequestConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blood_request'
--- Theyazan/Labs/lab_02/web_banking/blood_unit/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BloodUnitConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blood_unit'
