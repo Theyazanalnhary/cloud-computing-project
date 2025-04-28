a47df5f26312263587f93ae53a560491ea7fa1f7
Theyazanalnhary <dhyyznqasm23@gmail.com>
2025-04-29 00:42:39 +0300

    Delete Theyazan/Labs/lab_02/SchoolManagement directory

--- Theyazan/Labs/lab_02/SchoolManagement/students/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.student_list, name='student_list'),
-    path('<int:student_id>/edit/', views.student_edit, name='student_edit'),
-    path('<int:student_id>/delete/', views.student_delete, name='student_delete'),
-]
